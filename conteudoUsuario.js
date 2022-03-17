8ad3f7e9d557759fce1e6a134e643e5327fc02ff
stanescon <stanescon.n@gmail.com>
2022-03-17 03:17:08 -0300

    começando a incluir os invoices

--- conteudoUsuario.js
+++ conteudoUsuario.js
@@ -17,3 +17,45 @@ fetch(`https://chs-invoice-app-be.herokuapp.com/user/${userId}`)
 
 
 const novaFatura = document.querySelector('.botao-nova-fatura')
+const caixaNovaFatura = document.querySelector('.caixa-novo-invoice')
+const fundoEscuto = document.querySelector('.fundo-escuro')
+const botaoDescartarNovaFatura = document.querySelector('.descartar-invoice-novo')
+const botaoSalvarNovaFatura = document.querySelector('.salvar-invoice-novo')
+const inputNomeFaturaNova = document.querySelector('#nome-invoice-novo')
+const inputValorFaturaNova = document.querySelector('#valor-invoice-novo')
+const inputVencimentoFaturaNova = document.querySelector('#data-vencimento-novo')
+const statusFaturaNova = document.querySelector('.status-novo-invoice')
+
+novaFatura.onclick = function(){
+    fundoEscuto.classList.remove('oculto')
+    caixaNovaFatura.classList.remove('oculto')
+}
+botaoDescartarNovaFatura.onclick = function(){
+    inputNomeFaturaNova.value = ""
+    inputValorFaturaNova.value = ""
+    inputVencimentoFaturaNova.value = ""
+    statusFaturaNova.value = 'Pendente'
+    fundoEscuto.classList.add('oculto')
+    caixaNovaFatura.classList.add('oculto')
+}
+botaoSalvarNovaFatura.onclick = function(){
+    if(inputNomeFaturaNova.value && inputValorFaturaNova.value && inputVencimentoFaturaNova.value){
+        const headers = new Headers();
+            headers.append('Content-Type', 'application/json');
+            fetch('https://chs-invoice-app-be.herokuapp.com/invoices', {
+                headers,
+                method: 'POST',
+                body: JSON.stringify({
+                    userId: userId,
+                    invoiceName: inputNomeFaturaNova.value,
+                    invoiceValue: inputValorFaturaNova.value,
+                    invoiceDate: inputVencimentoFaturaNova.value,
+                    invoiceStatus: statusFaturaNova.value
+                })
+            }).then(resposta => resposta.json()).then(resposta => {
+                console.log(resposta)
+            })
+    }
+}
+
+
