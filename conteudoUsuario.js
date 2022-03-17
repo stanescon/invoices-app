cdc6b4c49884b0f6f4e1ea709ceb3e2b70f2e528
stanescon <stanescon.n@gmail.com>
2022-03-16 23:25:06 -0300

    incluindo telas de nova fatura

--- conteudoUsuario.js
+++ conteudoUsuario.js
@@ -3,3 +3,17 @@ if(window.innerWidth < 650) {
     document.querySelector('.botao-nova-fatura').innerHTML = '<img src="./imagens/icon+.png" class="icon-mais"> Nova';
     document.querySelector('.texto-filtro').innerHTML = 'Filtrar'
 }
+
+const userId = localStorage.getItem('userId')
+console.log(userId)
+
+
+// BUSCANDO INVOICES POR ID DO USUARIO
+fetch(`https://chs-invoice-app-be.herokuapp.com/user/${userId}`)
+.then(resposta => resposta.json()).then(resposta => {
+    console.log(resposta)
+})
+
+
+
+const novaFatura = document.querySelector('.botao-nova-fatura')
